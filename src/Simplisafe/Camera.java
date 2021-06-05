5d5ca949fae4f60ea16f76f018b5dea2582b1433
Mohjeet Singh <singhmohjeet1@gmail.com>
2021-06-05 13:01:14 -0700

    Added security camera class

---
+++ src/Simplisafe/Camera.java
@@ -0,0 +1,11 @@
+package Simplisafe;
+
+import Simplisafe.Class.Sensor;
+
+public class Camera extends Sensor {
+    private static final String sensorType = "Camera";
+
+    public Camera() {
+        super(sensorType);
+    }
+}
